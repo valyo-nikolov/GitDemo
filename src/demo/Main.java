a9102298c39ad9d4774ff05dbce29357341d8c2b
Valentin Nikolov <valentin.nikolov@lab08.com>
2022-05-18 09:49:55 +0300

    init tests

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -3,6 +3,5 @@ package demo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git changed once again!");
-        System.out.println("Hello new branch");
     }
 }
---
+++ src/lections/fourth/ArithmeticOperations.java
@@ -0,0 +1,15 @@
+package lections.fourth;
+
+public class ArithmeticOperations {
+    public static void main(String args[]) {
+        int num1 = 100;
+        int num2 = 20;
+
+        System.out.println("num1 + num2: " + (num1 + num2) );
+        System.out.println("num1 - num2: " + (num1 - num2) );
+        System.out.println("num1 * num2: " + (num1 * num2) );
+        System.out.println("num1 / num2: " + (num1 / num2) );
+        System.out.println("num1 % num2: " + (num1 % num2) );
+    }
+}
+
